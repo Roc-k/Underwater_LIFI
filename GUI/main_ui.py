4057ea4a0f29303ccb280699c0cc7a0d149f14c9
Roc-k <thematthewrock@gmail.com>
2025-09-18 15:14:11 -0400

    added radiobuttons and plots, add signals later

--- GUI/main_ui.py
+++ GUI/main_ui.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication, QWidget, QGridLayout
 from incoming_data_widget import IncomingDataWidget
 
 from outgoing_data_widget import OutgoingDataWidget
+from metric_widget import MetricWidget
 # from outgoing_metric_widget import OutgoingDataMetricWidget
 # from incoming_metric_widget import IncomingDataMetricWidget
 import sys
@@ -14,8 +15,8 @@ grid_layout = QGridLayout()
 
 grid_layout.addWidget(OutgoingDataWidget(), 0, 0)
 grid_layout.addWidget(IncomingDataWidget(), 0, 1)
-# grid_layout.addWidget(OutgoingDataMetricWidget(), 0, 1)
-# grid_layout.addWidget(IncomingDataMetricWidget(), 1, 1)
+grid_layout.addWidget(MetricWidget(), 1, 0)
+grid_layout.addWidget(MetricWidget(), 1, 1)
 
 window.setLayout(grid_layout)
 
--- GUI/metric_widget.py
+++ GUI/metric_widget.py
@@ -1,4 +1,20 @@
 from PySide6.QtWidgets import QRadioButton, QWidget, QGridLayout
+from pyqtgraph import PlotWidget, plot
+
+class DataRadioButton(QRadioButton):
+    def __init__(self, label:str, units:str):
+        super().__init__()
+
+
+        self.label = label
+        self.units = units
+        self.data = 0
+        self.update_data(self.data)
+
+    def update_data(self, new_data: float):
+        self.data = new_data
+        self.setText(f"{self.label} {self.data:.3f} {self.units}")
+
 
 
 class MetricWidget(QWidget):
@@ -6,3 +22,10 @@ class MetricWidget(QWidget):
         super().__init__()
         self.grid_layout = QGridLayout()
         self.setLayout(self.grid_layout)
+
+        self.data_radio_2 = DataRadioButton("ThroughRate:", "Mb/s")
+        self.data_radio = DataRadioButton("PacketLoss", "Packets/s")
+        self.grid_layout.addWidget(self.data_radio, 0, 0)
+        self.grid_layout.addWidget(self.data_radio_2, 1, 0)
+        self.plot = PlotWidget()
+        self.grid_layout.addWidget(self.plot, 0, 1, 2, 1)
