126aaec501bf615f535efc46464a8473d6c67abd
Roc-k <thematthewrock@gmail.com>
2025-09-24 19:20:36 -0400

    ready for initial connection test

--- Physical_Layer/ESP32test5/src/main.c
+++ Physical_Layer/ESP32test5/src/main.c
@@ -4,6 +4,8 @@ void app_main(void)
 {
     gpio_config(&io_led);
     gpio_config(&io_button);
+    gpio_config(&io_led_2);
+    gpio_config(&io_button_2);
     gpio_config(&io_input);
     gpio_config(&io_output);
     esp_task_wdt_init(&twdt_config); // only init in main thread
@@ -11,18 +13,174 @@ void app_main(void)
     esp_task_wdt_add(NULL);
 
     int lastButtonState = LOW;
-    while (1) {
-
-        // pet watchdog
-        esp_task_wdt_reset();
-
-        // Turn all LEDs ON
-        int buttonState = gpio_get_level(BUTTON_PIN_1);
-        int inputState = gpio_get_level(SIGNAL_INPUT_1);
-
-        gpio_set_level(SIGNAL_OUTPUT_1, buttonState)
-        gpio_set_level(LED_PIN_1, inputState)
-        
-        vTaskDelay(pdMS_TO_TICKS(100)); // let other tasks run
-};
+    sync_loop();
+    main_loop();
 }
+
+void sync_loop(void){
+    while (1) {
+        int button1 = gpio_get_level(BUTTON_PIN_1);
+        int button2 = gpio_get_level(BUTTON_PIN_2);
+
+        if(button1 == HIGH){
+            sync_transmitter();
+            return;
+        }
+
+        if(button2 == HIGH){
+            sync_receiver();
+            return;
+        }
+
+    }
+}
+
+void main_loop(void) {
+    while(1) {
+        int command = read();
+        if(command == TRANSMISSIONSIG){
+            int executable = receieve();
+            execute(executable);
+        }
+        if(gpio_get_level(BUTTON_PIN_1) == HIGH){
+            transmit(TRANSMISSIONSIG);
+            transmit_message(100);
+        }
+    }
+}
+
+void execute(int executable) {
+    switch (executable) {
+        case 100:
+            toggle();
+            break;
+    }
+}
+
+void toggle(){
+    int led_state = gpio_get_level(LED_PIN_2);
+    if(led_state == HIGH) {
+        write(LOW);
+    } else {
+        write(HIGH);
+    }
+}
+
+void start_sync(void){
+    write(HIGH);
+    int inputState;
+    do {
+        inputState = gpio_get_level(SIGNAL_INPUT_1);
+    } while (inputState == LOW);
+
+    //we've read a signal, time to turn off
+    write(LOW);
+
+    while(inputState == HIGH) {
+        //pause while they also turn off
+    }
+}
+
+void sync_transmitter(void){
+    start_sync();
+    do {
+        transmit(TRANSMISSIONSIG);
+    } while(gpio_get_level(SIGNAL_INPUT_1) == LOW);
+}
+
+int read(void) {
+    int read = 0;
+    int read_bits = 8;
+    while(read_bits > 0) {
+        read_bits -= 1;
+        int small_read = gpio_get_level(SIGNAL_INPUT_1);
+        read += small_read << read_bits;
+        sleep(CLOCKTIME);
+    }
+
+}
+
+void sync_find(void) {
+    // syncs such that terminates just after TRANSMISSIONSIG is read
+    int reading;
+    //wait for one high and low to beging searching(mini sync)
+    
+    while(gpio_get_level(SIGNAL_INPUT_1) == HIGH){
+
+    }
+    while(gpio_get_level(SIGNAL_INPUT_1) == LOW){
+
+    }
+
+    int reading;
+    reading = read();
+    while(reading != TRANSMISSIONSIG) {
+        //offset by 1 cycle, this can be made smarter later
+        sleep(CLOCKTIME);
+        reading = read();
+    }
+
+}
+
+void transmit(int signal) {
+    int send_ints = 8;
+    while(send_ints > 0) {
+        send(signal & 1);
+        signal >> 1;
+        send_ints -= 1;
+    }
+}
+
+void transmit_message(int signal){
+    while(gpio_get_level(SIGNAL_INPUT_1) == LOW) {
+        transmit(TRANSMISSIONSIG);
+    }
+    while(signal > 0){
+        transmit(signal);
+        signal >> 8;
+    }
+    transmit(TRANSMISSIONSIG);
+}
+
+int receieve(){
+
+    int start = read();
+    if (start != TRANSMISSIONSIG) {
+        sync_find();
+    }
+    //tell them we're ready
+    write(HIGH);
+
+    int message = 0;
+    int message_length = 0;
+    int current;
+    do {
+        current = read();
+        if(current == TRANSMISSIONSIG && message_length == 0){
+            current = 0;
+        }
+        message += current << 8 * message_length;
+        message_length += 1;
+
+    } while(current != TRANSMISSIONSIG);
+
+}
+
+void write(int output){
+    digitalWrite(SIGNAL_OUTPUT_1, output);
+    digitalWrite(LED_PIN_1, output);
+}
+
+void send(int singal) {
+    write(signal);
+    sleep(CLOCKTIME);
+}
+
+void sync_receiver(void){
+    start_sync();
+    sync_find();
+    write(HIGH);
+    sleep(CLOCKTIME * 2);
+    write(LOW);
+}
+
--- Physical_Layer/ESP32test5/src/main.h
+++ Physical_Layer/ESP32test5/src/main.h
@@ -5,16 +5,33 @@
 #include "sdkconfig.h"
 #include "esp_task_wdt.h" // watchdog
 
+#include "pthread.h"
+
+
 #define HIGH 1
 #define LOW 0
+#define CLOCKTIME 1
 
-#define LED_PIN_1 12
+#define TRANSMISSIONSIG 185
+
+#define LED_PIN_1 14
+#define LED_PIN_2 13
+
 #define BUTTON_PIN_1 16
-#define SIGNAL_INPUT_1 39
-#define SIGNAL_OUTPUT_1 6
+#define BUTTON_PIN_2 17
+#define SIGNAL_INPUT_1 40
+#define SIGNAL_OUTPUT_1 41
 
 gpio_config_t io_led = {
     .pin_bit_mask = (1ULL << LED_PIN_1),
+    .mode = GPIO_MODE_OUTPUT,              
+    .pull_up_en = GPIO_PULLUP_DISABLE,     
+    .pull_down_en = GPIO_PULLDOWN_DISABLE, 
+    .intr_type = GPIO_INTR_DISABLE         // Disable interrupts
+};
+
+gpio_config_t io_led_2 = {
+    .pin_bit_mask = (1ULL << LED_PIN_2),
     .mode = GPIO_MODE_OUTPUT,              
     .pull_up_en = GPIO_PULLUP_DISABLE,     
     .pull_down_en = GPIO_PULLDOWN_DISABLE, 
@@ -46,6 +63,13 @@ gpio_config_t io_button = {
     .intr_type = GPIO_INTR_DISABLE         // Disable interrupts
 };
 
+gpio_config_t io_button_2 = {
+    .pin_bit_mask = (1ULL << BUTTON_PIN_2),
+    .mode = GPIO_MODE_INPUT,               
+    .pull_up_en = GPIO_PULLUP_DISABLE,     
+    .pull_down_en = GPIO_PULLDOWN_DISABLE, 
+    .intr_type = GPIO_INTR_DISABLE         // Disable interrupts
+};
 
 esp_task_wdt_config_t twdt_config = {
     .timeout_ms = 5000,          
